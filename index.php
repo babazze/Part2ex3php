e01ad932089977cedc6b66935e750b0da6013064
babazze <babazze@live.fr>
2019-12-04 14:25:32 +0100

    corrigé

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <?php
-$sexe = 'Femme';
+$gender = 'Homme';
 ?>
 <!DOCTYPE html>
 <html lang="fr">
   <head>
     <meta charset="utf-8" />
-    <title>ex5</title>
+    <title>ex3</title>
   </head>
   <body>
       <p>
+        <?php if ($gender != 'Homme') { ?>
+        C'est une développeuse !!!
+        <?php } else { ?>
+        C'est un développeur !!!
+        <?php } ?>
+        <!-- peux aussi s'ecrir :  -->
         <?php
-        if ($sexe == 'Femme') {
-        echo 'C\'est une développeuse !!!';
-        } else {
-        echo 'C\'est un développeur !!!';
-        }
+        echo ($gender != 'Homme') ? 'C\'est une développeuse !!!' : 'C\'est un développeur !!!';
+        /*  ce qui est avant le '?' corréspond à la condition
+        ce qui est apres le '?' corréspond au if et else, ils sont séparé par les ' : '
+        '?' sépare les conditions des instructions. */
         ?>
       </p>
   </body>
